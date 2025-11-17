7d464bd65237eb3821d3cde7bf5656c7fdbe18f1
arishhaque <arish.haq@gmail.com>
2025-11-17 15:05:49 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        {/* Your content goes here */}
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+  return (
+    <>
+      <header className="header-container">
+        <h1>Welcome to Endgame App</h1>
+        <p>Guess the word under 8 attemps to keep the programming world safe from Assembly</p>
+      </header>
+    </>
+  );
+}
+
+export default Header;
